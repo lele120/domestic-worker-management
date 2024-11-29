700485a7931cd363e8189f68492f92b9ef50f68d
Emanuele Travanti <emanueletravanti@MacBook-Pro-de-Emanuele.local>
2024-11-28 19:58:44 -0500

    refactor structure of project

--- next.config.mjs
+++ next.config.mjs
@@ -2,27 +2,12 @@ import createNextIntlPlugin from 'next-intl/plugin';
  
 const withNextIntl = createNextIntlPlugin();
  
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     output: 'export',
-//     images: {
-//       unoptimized: true,
-//       remotePatterns: [
-//         {
-//           protocol: 'https',
-//           hostname: 'images.unsplash.com',
-//         },
-//       ],
-//     },
-//   }
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {reactStrictMode: true,
     images: {
       domains: ['images.unsplash.com'],
     },
-    i18n: {
-      locales: ['en', 'es', 'it'],
-      defaultLocale: 'en',
-    },};
+   };
  
 export default withNextIntl(nextConfig);
